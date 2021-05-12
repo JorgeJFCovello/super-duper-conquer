4000ad5c3beb9390e194d813dc098d79163417ee
Jorge Julio Fernando Covello <jorgejfcovello@gmail.com>
2021-05-12 00:56:16 -0300

    agrego vuetify

---
+++ FrontEnd/super-duper-conquer/components/login/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div >
+    <v-card raised class="center_class" width="50%" height="50%"  title="welcome">
+      <v-text-field label="Username!" title="username" v-model="username" @enter="enterInSystem(username)"/>
+      <v-btn mx-2 @click="enterInSystem(username)">
+          log in!
+      </v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return { 
+            username: null
+        }
+    },
+    methods: {
+
+        enterInSystem(value) {
+            return value;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.center_class {
+  margin: 20%;
+  border: 3px solid green;
+  padding: 10px;
+}
+</style>
