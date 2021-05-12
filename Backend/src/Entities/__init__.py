af53ecb86a36866905fd0cf9ca670ddd212e527d
Jorge Julio Fernando Covello <jorgejfcovello@gmail.com>
2021-05-11 23:47:54 -0300

    deleteo backend

--- Backend/src/Entities/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.Servicies import get_map_constructor
-
-
-class game():
-
-    def __init__(self):
-        self.countries = get_map_constructor().get_countries()
-        self.players = {}
-
