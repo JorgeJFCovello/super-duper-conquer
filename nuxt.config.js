b6e8bfbc8cf6a40b53640d5b5f4ebef346f27814
JorgeJFCovello <jorgejfcovello@gmail.com>
2021-09-08 17:16:52 -0300

    fix heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,7 @@ export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     target: 'static',
     server: {
-        port: process.env.PORT || 5000,
-        host: process.env.HOST || 'localhost'
+        port: process.env.PORT || 5000
     },
     router: {
         base: '/super-duper-conquer/'
