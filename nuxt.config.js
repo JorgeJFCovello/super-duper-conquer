42e81c390b6279949b25ed76c38cb4e9b5fd209c
JorgeJFCovello <jorgejfcovello@gmail.com>
2021-09-08 17:29:13 -0300

    FIX2

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,8 @@ export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     target: 'static',
     server: {
-        port: process.env.PORT || 5000
+        port: process.env.PORT || 5000,
+        host: process.env.HOST || 'localhost'
     },
     router: {
         base: '/'
